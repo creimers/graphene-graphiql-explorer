f685ea08ef766563dcc40544227f019effc636eb
Christoph Reimers <christoph@superservice-international.com>
2019-07-01 15:03:42 +0200

    up and running (looking @ localhost:8000)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,162 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import GraphiQL from "graphiql";
+import GraphiQLExplorer from "graphiql-explorer";
+import { buildClientSchema, getIntrospectionQuery, parse } from "graphql";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+import { makeDefaultArg, getDefaultScalarArgValue } from "./CustomArgs";
+
+import "graphiql/graphiql.css";
+import "./App.css";
+
+function fetcher(params) {
+  return fetch(
+    "http://localhost:8000/graphql",
+    {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(params)
+    }
+  )
+    .then(function(response) {
+      return response.text();
+    })
+    .then(function(responseBody) {
+      try {
+        return JSON.parse(responseBody);
+      } catch (e) {
+        return responseBody;
+      }
+    });
+}
+
+const DEFAULT_QUERY = `{}`;
+
+class App extends Component {
+  _graphiql;
+  state = { schema: null, query: DEFAULT_QUERY, explorerIsOpen: true };
+
+  componentDidMount() {
+    fetcher({
+      query: getIntrospectionQuery()
+    }).then(result => {
+      const editor = this._graphiql.getQueryEditor();
+      editor.setOption("extraKeys", {
+        ...(editor.options.extraKeys || {}),
+        "Shift-Alt-LeftClick": this._handleInspectOperation
+      });
+
+      this.setState({ schema: buildClientSchema(result.data) });
+    });
+  }
+
+  _handleInspectOperation = (cm, mousePos) => {
+    const parsedQuery = parse(this.state.query || "");
+
+    if (!parsedQuery) {
+      console.error("Couldn't parse query document");
+      return null;
+    }
+
+    var token = cm.getTokenAt(mousePos);
+    var start = { line: mousePos.line, ch: token.start };
+    var end = { line: mousePos.line, ch: token.end };
+    var relevantMousePos = {
+      start: cm.indexFromPos(start),
+      end: cm.indexFromPos(end)
+    };
+
+    var position = relevantMousePos;
+
+    var def = parsedQuery.definitions.find(definition => {
+      if (!definition.loc) {
+        console.log("Missing location information for definition");
+        return false;
+      }
+
+      const { start, end } = definition.loc;
+      return start <= position.start && end >= position.end;
+    });
+
+    if (!def) {
+      console.error(
+        "Unable to find definition corresponding to mouse position"
+      );
+      return null;
+    }
+
+    var operationKind =
+      def.kind === "OperationDefinition"
+        ? def.operation
+        : def.kind === "FragmentDefinition"
+        ? "fragment"
+        : "unknown";
+
+    var operationName =
+      def.kind === "OperationDefinition" && !!def.name
+        ? def.name.value
+        : def.kind === "FragmentDefinition" && !!def.name
+        ? def.name.value
+        : "unknown";
+
+    var selector = `.graphiql-explorer-root #${operationKind}-${operationName}`;
+
+    var el = document.querySelector(selector);
+    el && el.scrollIntoView();
+  };
+
+  _handleEditQuery = query => this.setState({ query });
+
+  _handleToggleExplorer = () => {
+    this.setState({ explorerIsOpen: !this.state.explorerIsOpen });
+  };
+
+  render() {
+    const { query, schema } = this.state;
+    return (
+      <div className="graphiql-container">
+        <GraphiQLExplorer
+          schema={schema}
+          query={query}
+          onEdit={this._handleEditQuery}
+          onRunOperation={operationName =>
+            this._graphiql.handleRunQuery(operationName)
+          }
+          explorerIsOpen={this.state.explorerIsOpen}
+          onToggleExplorer={this._handleToggleExplorer}
+          getDefaultScalarArgValue={getDefaultScalarArgValue}
+          makeDefaultArg={makeDefaultArg}
+        />
+        <GraphiQL
+          ref={ref => (this._graphiql = ref)}
+          fetcher={fetcher}
+          schema={schema}
+          query={query}
+          onEditQuery={this._handleEditQuery}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <GraphiQL.Toolbar>
+            <GraphiQL.Button
+              onClick={() => this._graphiql.handlePrettifyQuery()}
+              label="Prettify"
+              title="Prettify Query (Shift-Ctrl-P)"
+            />
+            <GraphiQL.Button
+              onClick={() => this._graphiql.handleToggleHistory()}
+              label="History"
+              title="Show History"
+            />
+            <GraphiQL.Button
+              onClick={this._handleToggleExplorer}
+              label="Explorer"
+              title="Toggle Explorer"
+            />
+          </GraphiQL.Toolbar>
+        </GraphiQL>
+      </div>
+    );
+  }
 }
 
 export default App;
